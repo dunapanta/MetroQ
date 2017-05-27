f4c8584adc7810d58107437123fa5a91d8e20d52
Daniel Unapanta <dunapanta@udlanet.ec>
2017-05-26 22:23:30 -0500

    Login Finalizado

--- app/src/main/java/com/projecto/metroq/metroq/views/CreateAccountActivity.java
+++ app/src/main/java/com/projecto/metroq/metroq/views/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.projecto.metroq.metroq.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.projecto.metroq.metroq.R;
 
@@ -11,5 +12,16 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+            showToolbar(getResources().getString(R.string.toolbar_title_create_account), true);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
     }
 }
